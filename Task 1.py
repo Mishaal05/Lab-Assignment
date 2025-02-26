1dae1a8238b653a40ee2fdbf16df69f75be79e3a
Mishaal Fatima <148870549+Mishi26@users.noreply.github.com>
2025-02-26 22:40:21 +0500

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,20 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Numbers: 5, 10, 15, 20")
+Numbers: 5, 10, 15, 20
+>>> print(64 + 32)
+96
+>>> x = 64
+>>> y = 32
+>>> sum = x + y
+>>> print(sum)
+96
+>>> s='Lucky'
+>>> print(s)
+Lucky
+>>> print("Today is 26/02/2025")
+Today is 26/02/2025
+>>> phonenumber=input('Your phone number is: ')
+Your phone number is: 03004826050
+>>> print(phonenumber)
+03004826050
